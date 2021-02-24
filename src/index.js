113d721a5a5956fa92b5dbe6c66cfad0e481d14f
Aleksander Voronov <irreligious86@gmail.com>
2021-02-24 21:13:53 +0100

    feat: add basic functional

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 
 exports.min = function min (array) {
-  return 0;
+    if (!array) return 0;
+    if (!array.length) return 0;
+    return Math.min.apply(null, array);
 }
 
 exports.max = function max (array) {
-  return 0;
+    if (!array) return 0;
+    if (!array.length) return 0;
+    return Math.max.apply(null, array);
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if (!array) return 0;
+    if (!array.length) return 0;
+    return array.reduce( (acc, prev) => acc+prev, 0 ) /array.length ;
+
 }
